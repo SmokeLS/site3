a7ca7d4dd785809c340ffa8caea55af8043abd0a
Andrey <smokels1998@gmail.com>
2020-08-30 16:31:36 +0300

    now i support IE11

---
+++ js/script.js
@@ -0,0 +1,46 @@
+ (function( $ ){
+    $.fn.check = function(block) {
+        if ($(block).hasClass("disable"))
+            return false;
+        $(block).siblings('.default-subtitle').toggle();
+        $(block).siblings('.selected-subtitle').toggle();
+    }; 
+ })( jQuery );
+ (function( $ ){
+    $(".disable").siblings('.default-subtitle').toggle();
+    $(".disable").siblings('.disabled-subtitle').toggle();
+ })( jQuery );
+$( document ).ready(function() {
+    $(".section-block").on("click",function(event){
+        if ($(event.target.closest("selected")) && $(this).hasClass("selected")){
+            $(this).check(this);
+            $(this).removeClass("selected");
+            return false;
+        }
+        if ($(this).data("selected") == 1){
+            $(this).check(this);
+            $(this).toggleClass("selectedHover");
+            return false;
+        }
+        $(this).addClass("selected");
+        $(this).check(this);
+        $(event.target).closest(this).addClass("hover");
+        $(this).data("selected",1);
+    });
+});
+
+/*     
+
+         &-block
+            border-right: 4px solid $default-color
+            border-bottom: 4px solid $default-color;
+            &__wrap
+            position: relative
+                &-header
+                    border: 4px solid $default-color
+                    border-left: 6px solid $default-color
+                &-main
+                    border-left: 4px solid $default-color;
+                
+                
+                */
